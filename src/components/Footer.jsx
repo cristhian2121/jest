b1dd959d5f0929a90748a7c36908e296c5f38180
maestro_roshi <ccdelgadop@gmail.com>
2021-07-10 12:28:11 -0500

    Change theme to Dragon ball team

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import '../styles/components/Footer.styl';
 
 const Footer = () => (
   <div className="Footer class-ezample-snapshot">
-    <p className="Footer-title">Platzi Store</p>
-    <p className="Footer-copy">Todos los Izquierdos Reservados</p>
+    <p className="Footer-title">Kame Store</p>
+    <p className="Footer-copy">Todos los Izquierdos Reservados @KameHouse</p>
   </div>
 );
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = props => (
   <div className="Header">
     <h1 className="Header-title">
       <Link to="/">
-        Platzi Store
+        Kame Store
       </Link>
     </h1>
     <div className="Header-checkout">
